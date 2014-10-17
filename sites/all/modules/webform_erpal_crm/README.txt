e9cc25f48e289e0ca06db0f8ee76186d0e70f2cd
btribulski <briant@loftproductions.com>
2014-10-17 13:38:11 -0400

    Updated site with ERPAL integration

---
+++ sites/all/modules/webform_erpal_crm/README.txt
@@ -0,0 +1,2 @@
+Enable an 'erpal_crm_service' module to work with it on local or remote site.
+Set its domain in 'Contact' and 'Activity' services URLs on the ERPAL Webform tab.
--- sites/all/themes/paintingcompany/block-block-3.tpl.php
+++
@@ -1,4 +0,0 @@
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?>">
-  <?php print $block->content; ?>
-</div>
-<!-- /#block-<?php print $block->module .'-'. $block->delta; ?> -->
